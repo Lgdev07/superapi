184fda1e5c0d4c2fc1a45928171cf15217f86d16
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-08-16 13:13:01 -0300

    [NEW] Add new routes and comment to InitializeRoutes

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -4,11 +4,12 @@ import (
 	"github.com/Lgdev07/superapi/api/middlewares"
 )
 
+// InitializeRoutes set the routes and middlewares for the Router
 func (s *Server) InitializeRoutes() {
 	s.Router.Use(middlewares.SetContentTypeMiddleware)
 
-	s.Router.HandleFunc("/index", s.Index).Methods("GET")
 	s.Router.HandleFunc("/supers", s.CreateSuper).Methods("POST")
 	s.Router.HandleFunc("/supers", s.ListSupers).Methods("GET")
-	s.Router.HandleFunc("/supers/{id:[0-9]+}", s.Index).Methods("DELETE")
+	s.Router.HandleFunc("/supers/{id:[a-z0-9-]+}", s.ShowSuper).Methods("GET")
+	s.Router.HandleFunc("/supers/{id:[a-z0-9-]+}", s.DeleteSuper).Methods("DELETE")
 }
